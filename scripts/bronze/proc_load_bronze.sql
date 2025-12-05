bda6dd6f1d49f5f99829df9ccbd716711bb0e7df
Harshit Pandya <harshitpandya1809@gmail.com>
2025-12-05 22:11:06 +0530

    Update proc_load_bronze.sql

--- scripts/bronze/proc_load_bronze.sql
+++ scripts/bronze/proc_load_bronze.sql
@@ -33,7 +33,7 @@ EXEC bronze.load_bronze
 
             TRUNCATE TABLE bronze.crm_cust_info;
             BULK INSERT bronze.crm_cust_info
-            FROM 'C:\Users\Harshit\OneDrive\Desktop\SQL_Project\sql-data-warehouse-project\datasets\source_crm\cust_info.csv'
+            FROM 'datasets\source_crm\cust_info.csv'
             WITH (FIRSTROW = 2, FIELDTERMINATOR = ',', TABLOCK);
 
             SET @end_time = GETDATE();
@@ -46,7 +46,7 @@ EXEC bronze.load_bronze
 
             TRUNCATE TABLE bronze.crm_prd_info;
             BULK INSERT bronze.crm_prd_info
-            FROM 'C:\Users\Harshit\OneDrive\Desktop\SQL_Project\sql-data-warehouse-project\datasets\source_crm\prd_info.csv'
+            FROM 'datasets\source_crm\prd_info.csv'
             WITH (FIRSTROW = 2, FIELDTERMINATOR = ',', TABLOCK);
 
             SET @end_time = GETDATE();
@@ -59,7 +59,7 @@ EXEC bronze.load_bronze
 
             TRUNCATE TABLE bronze.crm_sales_details;
             BULK INSERT bronze.crm_sales_details
-            FROM 'C:\Users\Harshit\OneDrive\Desktop\SQL_Project\sql-data-warehouse-project\datasets\source_crm\sales_details.csv'
+            FROM 'datasets\source_crm\sales_details.csv'
             WITH (FIRSTROW = 2, FIELDTERMINATOR = ',', TABLOCK);
 
             SET @end_time = GETDATE();
@@ -80,7 +80,7 @@ EXEC bronze.load_bronze
 
             TRUNCATE TABLE bronze.erp_cust_az12;
             BULK INSERT bronze.erp_cust_az12
-            FROM 'C:\Users\Harshit\OneDrive\Desktop\SQL_Project\sql-data-warehouse-project\datasets\source_erp\cust_az12.csv'
+            FROM 'datasets\source_erp\cust_az12.csv'
             WITH (FIRSTROW = 2, FIELDTERMINATOR = ',', TABLOCK);
 
             SET @end_time = GETDATE();
@@ -93,7 +93,7 @@ EXEC bronze.load_bronze
 
             TRUNCATE TABLE bronze.erp_loc_a101;
             BULK INSERT bronze.erp_loc_a101
-            FROM 'C:\Users\Harshit\OneDrive\Desktop\SQL_Project\sql-data-warehouse-project\datasets\source_erp\loc_a101.csv'
+            FROM 'datasets\source_erp\loc_a101.csv'
             WITH (FIRSTROW = 2, FIELDTERMINATOR = ',', TABLOCK);
 
             SET @end_time = GETDATE();
@@ -106,7 +106,7 @@ EXEC bronze.load_bronze
 
             TRUNCATE TABLE bronze.erp_px_cat_g1v2;
             BULK INSERT bronze.erp_px_cat_g1v2
-            FROM 'C:\Users\Harshit\OneDrive\Desktop\SQL_Project\sql-data-warehouse-project\datasets\source_erp\px_cat_g1v2.csv'
+            FROM 'datasets\source_erp\px_cat_g1v2.csv'
             WITH (FIRSTROW = 2, FIELDTERMINATOR = ',', TABLOCK);
 
             SET @batch_end_time = GETDATE();
